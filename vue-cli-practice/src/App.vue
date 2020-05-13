d28260d50c0f545d28dac224def246226c26291b
JL Reyes <joseluisreyes43@gmail.com>
2020-05-13 15:07:20 +0800

    created components from a sample file provided by the course with corresponding subfolders

---
+++ vue-cli-practice/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container">
+        <!-- Header component -->
+        <jl-header></jl-header>
+        <hr>
+        <div class="row">
+            <!-- ServerList component -->
+            <jl-server-list></jl-server-list>
+            <!-- ServerDetails component -->
+            <jl-server-details></jl-server-details>
+        </div>
+        <hr>
+        <div class="row">
+            <!-- ServerManage component -->
+            <jl-footer></jl-footer>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Header from './components/Shared/Header.vue'
+    import ServerList from './components/Server/ServerList.vue'
+    import ServerDetails from './components/Server/ServerDetails.vue'
+    import Footer from './components/Shared/Footer.vue'
+
+    export default{
+        components: {
+            'jl-header': Header,
+            'jl-server-list': ServerList,
+            'jl-server-details': ServerDetails,
+            'jl-footer': Footer
+        }
+    }
+</script>
+
+<style>
+
+</style>
