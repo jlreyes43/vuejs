c68430e90f9f3014891b9bf7ef1aa88d64411c24
JL Reyes <joseluisreyes43@gmail.com>
2020-05-19 16:01:11 +0800

    showed how styling and dynamic content works with slots

--- quotes/src/App.vue
+++ quotes/src/App.vue
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-xs-12">
                <jl-quote>
-                   <h2>The Quote</h2>
+                   <h2>{{quoteTitle}}</h2>
                    <p>A Wonderful Quote</p>
                </jl-quote>
             </div>
@@ -16,6 +16,11 @@
     import Quote from './components/Quote.vue'
 
     export default {
+        data: function(){
+            return{
+                quoteTitle: 'The Quote'
+            }
+        },
         components: {
             jlQuote: Quote
         }
