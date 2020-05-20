55cc1381170d23676101a76d35ef426f5ff38199
JL Reyes <joseluisreyes43@gmail.com>
2020-05-20 11:13:26 +0800

    created 2 new components Quotes and QuotesGrid. Passed data from App to QuotesGrid and used in a slot in Quotes

--- quotes_project/src/App.vue
+++ quotes_project/src/App.vue
@@ -1,12 +1,23 @@
 <template>
     <div class="container">
-    
+        <jl-quotes-grid :quotes="quotes"></jl-quotes-grid>
     </div>
 </template>
 
 <script>
+
+    import QuotesGrid from './components/QuotesGrid.vue'
+
     export default {
-        
+        data: function(){
+            return{
+                quotes: ['An eye for an eye makes the whole world blind'],
+                maxQuotes: 10
+            }
+        },
+        components:{
+            jlQuotesGrid: QuotesGrid
+        }
     }
 </script>
 
---
+++ quotes_project/src/components/QuotesGrid.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="row">
+        <jl-quotes v-for="quote in quotes">{{ quotes }}</jl-quotes>
+    </div>
+</template>
+
+<script>
+
+    import Quotes from './Quotes.vue'
+
+    export default {
+        props:[
+            'quotes'
+        ],
+        components:{
+            jlQuotes: Quotes
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
