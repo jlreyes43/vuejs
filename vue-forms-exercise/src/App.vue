a9e9e7751f6d59b66c7ed683db5a88e1a0c78ece
JL Reyes <joseluisreyes43@gmail.com>
2020-05-27 14:07:19 +0800

    completed exercise 1 & 2

--- vue-forms-exercise/src/App.vue
+++ vue-forms-exercise/src/App.vue
@@ -2,14 +2,58 @@
     <div class="container">
         <form>
             <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3" v-if="!isSubmitted">
                     <!-- Exercise 1 -->
                     <!-- Create a Signup Form where you retrieve the following Information -->
                     <!-- Full Name (First Name + Last Name) -->
                     <!-- Mail -->
                     <!-- Password -->
                     <!-- Store Data? Yes/No -->
+                    <br>
+                    <div class="form-group">
+                        <label for="Name">Name</label>
+                        <input 
+                            id="user.name"
+                            v-model="user.name"  
+                            type="text" 
+                            class="form-control">
+                    </div>
 
+                    <div class="form-group">
+                        <label for="mail">Mail</label>
+                        <input 
+                            id="user.mail" 
+                            v-model="user.mail" 
+                            type="text" 
+                            class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input 
+                            id="user.password" 
+                            v-model="user.password" 
+                            type="password" 
+                            class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="isStored">
+                            <input 
+                                id="isStored" 
+                                v-model="isStored" 
+                                value="isStored" 
+                                type="checkbox"> Store in database?
+                        </label>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                            <button
+                                    class="btn btn-primary"
+                                    @click.prevent="submitted">Submit!
+                            </button>
+                        </div>
+                    </div>
                     <!-- Exercise 2 -->
                     <!-- Only display the Form if it has NOT been submitted -->
                     <!-- Display the Data Summary ONCE the Form HAS been submitted -->
@@ -22,16 +66,16 @@
         </form>
         <hr>
         <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3" v-if="isSubmitted">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Full Name: </p>
-                        <p>Mail: </p>
-                        <p>Password: </p>
-                        <p>Store in Database?: </p>
+                        <p>Full Name: {{ user.name }}</p>
+                        <p>Mail:  {{ user.mail }}</p>
+                        <p>Password:  {{ user.password }} </p>
+                        <p>Store in Database?:  {{ isStored }}</p>
                     </div>
                 </div>
             </div>
@@ -41,6 +85,22 @@
 
 <script>
     export default {
+        data(){
+            return{
+                user:{
+                    name: '',
+                    mail: '',
+                    password: ''
+                },
+                isStored: false,
+                isSubmitted: false
+            }
+        },
+        methods:{
+            submitted(){
+                this.isSubmitted = true
+            }
+        }
     }
 </script>
 
