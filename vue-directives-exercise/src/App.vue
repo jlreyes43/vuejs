391b67b80c2e282294682a0febee5c8b2ed3f6a4
JL Reyes <joseluisreyes43@gmail.com>
2020-06-01 15:37:59 +0800

    finished the directives exercise, replicating v-on directive

--- vue-directives-exercise/src/App.vue
+++ vue-directives-exercise/src/App.vue
@@ -5,14 +5,47 @@
                 <h1>Directives Exercise</h1>
                 <!-- Exercise -->
                 <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+                <button v-jlOn:click="gotClicked" class="btn btn-primary">Click me</button>
+                <hr>
+                <div 
+                    style="width: 100px; height: 100px; background-color: lightgreen"
+                    v-jlOn:mouseenter="mouseEnter"
+                    v-jlOn:mouseleave="mouseLeaved">
 
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    export default {}
+    export default {
+        directives: {
+            jlOn: {
+                bind(el, binding){
+                    // el.onclick = function(){
+                    //     binding.value();
+                    // }
+
+                    const type = binding.arg;
+                    const func = binding.value;
+
+                    el.addEventListener(type, func);
+                }
+            }
+        },
+        methods:{
+            gotClicked(){
+                alert('you clicked it');
+            },
+            mouseEnter(){
+                console.log('Mouse entered!');
+            },
+            mouseLeaved(){
+                console.log('Mouse left!')
+            }
+        }
+    }
 </script>
 
 <style>
