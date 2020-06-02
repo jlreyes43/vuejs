ca591d015785ff430b3a64abff1d8052c8815d8a
JL Reyes <joseluisreyes43@gmail.com>
2020-06-02 16:48:08 +0800

    fixed routes properly adjusting the data per component based on the specific user id

--- vue-routing/src/components/Header.vue
+++ vue-routing/src/components/Header.vue
@@ -1,6 +1,7 @@
 <template>
     <ul class="nav nav-pills">
         <router-link to="/" tag="li" active-class="active" exact><a>Home</a></router-link>
-        <router-link to="/user/13" tag="li" active-class="active"><a>User</a></router-link>
+        <router-link to="/user/13" tag="li" active-class="active"><a>User 13</a></router-link>
+        <router-link to="/user/26" tag="li" active-class="active"><a>User 26</a></router-link>
     </ul>
 </template>
