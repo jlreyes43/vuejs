34de090ca183da07145620329eff5598d808d0d8
JL Reyes <joseluisreyes43@gmail.com>
2020-06-14 13:22:49 +0800

    made my first post to my firebase database

--- vue-resource/src/App.vue
+++ vue-resource/src/App.vue
@@ -29,7 +29,13 @@
         },
         methods: {
             submit(){
-                console.log(this.user);
+                //https://jls-vue-resource.firebaseio.com/
+                this.$http.post('https://jls-vue-resource.firebaseio.com/data.json', this.user)
+                    .then(response => {
+                        console.log(response)
+                    }, error => {
+                        console.log(error)
+                    });
             }
         }
     }
