de8f89063f0f12e3924f16d43732dfb693295998
JL Reyes <joseluisreyes43@gmail.com>
2020-06-14 15:09:49 +0800

    made a root request location globally

--- vue-resource/src/App.vue
+++ vue-resource/src/App.vue
@@ -37,7 +37,7 @@
         methods: {
             submit(){
                 //https://jls-vue-resource.firebaseio.com/
-                this.$http.post('https://jls-vue-resource.firebaseio.com/data.json', this.user)
+                this.$http.post('', this.user)
                     .then(response => {
                         console.log(response)
                     }, error => {
@@ -45,7 +45,7 @@
                     });
             },
             fetchData(){
-                this.$http.get('https://jls-vue-resource.firebaseio.com/data.json')
+                this.$http.get('')
                     .then(response => {
                         return response.json();
                     })
