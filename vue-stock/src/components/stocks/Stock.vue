e5b2929cd6962b7bfeeabd978bae8f5f0c2413d2
JL Reyes <joseluisreyes43@gmail.com>
2020-06-30 18:45:30 +0800

    set up stocks and stocks-stock files

--- vue-stock/src/components/stocks/Stock.vue
+++ vue-stock/src/components/stocks/Stock.vue
@@ -1,3 +1,19 @@
 <template>
-    <h1>Stocks Stock Component</h1>
+    <div class="col-sm-6 col-md-4">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Name
+                    <small>(Price: PRICE)</small>
+                </h3>
+            </div>
+            <div class="card-body">
+                <div class="float-left">
+                    <input type="number" class="form-control" placeholder="quantity">
+                </div>
+                <div class="float-right">
+                    <button class="btn btn-success">Buy</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
--- vue-stock/src/components/stocks/Stocks.vue
+++ vue-stock/src/components/stocks/Stocks.vue
@@ -1,3 +1,25 @@
 <template>
-    <h1>Stocks Component</h1>
+    <div class="row">
+        <app-stock v-for="stock in stocks"></app-stock>
+    </div>
 </template>
+
+<script>
+    import Stock from './Stock.vue';
+
+    export default {
+        data(){
+            return{
+                stocks: [
+                    { id: 1, name: 'Volkswagen', price: 50 },
+                    { id: 2, name: 'Google', price: 20 },
+                    { id: 3, name: 'Udemy', price: 40 },
+                    { id: 4, name: 'Twitter', price: 10 },
+                ]
+            }
+        },
+        components:{
+            appStock: Stock
+        }
+    }
+</script>
