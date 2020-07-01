3ea806e5a826c56addf4ea6413b5f99fcf8d1c0a
JL Reyes <joseluisreyes43@gmail.com>
2020-07-01 11:32:05 +0800

    setup vuex with the s,m,a,g and setup data folder to simulate database

--- vue-stock/src/components/stocks/Stocks.vue
+++ vue-stock/src/components/stocks/Stocks.vue
@@ -8,18 +8,13 @@
     import Stock from './Stock.vue';
 
     export default {
-        data(){
-            return{
-                stocks: [
-                    { id: 1, name: 'Volkswagen', price: 50 },
-                    { id: 2, name: 'Google', price: 20 },
-                    { id: 3, name: 'Udemy', price: 40 },
-                    { id: 4, name: 'Twitter', price: 10 },
-                ]
-            }
-        },
         components:{
             appStock: Stock
+        },
+        computed: {
+            stocks(){
+                return this.$store.getters.stocks;
+            }
         }
     }
 </script>
---
+++ vue-stock/src/data/stocks.js
@@ -0,0 +1,6 @@
+export default [
+    { id: 1, name: 'Volkswagen', price: 50 },
+    { id: 2, name: 'Google', price: 20 },
+    { id: 3, name: 'Udemy', price: 40 },
+    { id: 4, name: 'Twitter', price: 10 }
+];
