df69c74856a1057c73103b4332c85797807d05b9
JL Reyes <joseluisreyes43@gmail.com>
2020-07-01 11:17:53 +0800

    setup buying stock method

--- vue-stock/src/components/stocks/Stock.vue
+++ vue-stock/src/components/stocks/Stock.vue
@@ -2,18 +2,48 @@
     <div class="col-sm-6 col-md-4">
         <div class="card">
             <div class="card-header">
-                <h3 class="card-title">Name
-                    <small>(Price: PRICE)</small>
+                <h3 class="card-title">{{ stock. name }}
+                    <small>(Price: {{ stock.price }})</small>
                 </h3>
             </div>
             <div class="card-body">
                 <div class="float-left">
-                    <input type="number" class="form-control" placeholder="quantity">
+                    <input 
+                        type="number" 
+                        class="form-control" 
+                        placeholder="quantity"
+                        v-model="quantity">
                 </div>
                 <div class="float-right">
-                    <button class="btn btn-success">Buy</button>
+                    <button 
+                        class="btn btn-success"
+                        @click="buyStock"
+                        :disabled="quantity <= 0 || !Number.isInteger(+quantity)"
+                        >Buy</button>
                 </div>
             </div>
         </div>
     </div>
 </template>
+
+<script>
+    export default {
+        props: ['stock'],
+        data(){
+            return{
+                quantity: 0
+            }
+        },
+        methods: {
+            buyStock(){
+                const order = {
+                    stockId: this.stock.id,
+                    stockPrice: this.stock.price,
+                    quantity: this.quantity
+                };
+                console.log(order);
+                this.quantity = 0;
+            }
+        }
+    }
+</script>
